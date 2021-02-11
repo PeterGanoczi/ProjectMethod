152c6c481f7dba74666918d4061bca2de3c5c3ec
PeterGanoczi <peter.ganoczi@akademiasovy.sk>
2021-02-11 21:47:25 +0100

    initial commint in second branch

--- src/sk/itsovy/ganoczi/secondexample/Main.java
+++ src/sk/itsovy/ganoczi/secondexample/Main.java
@@ -3,6 +3,8 @@ package sk.itsovy.ganoczi.secondexample;
 public class Main {
     public static void main(String[] args) {
 
+        //Second branch peter
+
         MyInterface ref=Example::new;
         ref.print(10,5);
     }
