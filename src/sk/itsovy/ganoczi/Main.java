d289da7984e4218a5f220e4c3b133490edc1b410
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-16 22:42:49 +0100

    Update Main.java
    changed

--- src/sk/itsovy/ganoczi/Main.java
+++ src/sk/itsovy/ganoczi/Main.java
@@ -3,9 +3,9 @@ package sk.itsovy.ganoczi;
 public class Main {
     public static void main(String[] args) {
         Flower flower=new Flower();
+        // mame tu nejaky interface
+        MyInterface ref2=flower::printFlowerName;
 
-        MyInterface ref=flower::printFlowerName;
-
-        ref.print("Cactus");
+        ref2.print("Cactus");
     }
 }
